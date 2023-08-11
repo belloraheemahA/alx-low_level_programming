16ec2c20c3b070e08f603530bf9d15c0d7993f4b
belloraheemah <belloraheemah1@gmail.com>
2023-08-11 03:16:18 -0700

    puts and printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
 	 * Description- programming is like multilingual puzzle
 	 * Return: Always 0 (success)
 	 */
-int main(void);
-	{
-		puts("\"Programming is like building a multilingual puzzle\n");
-		return (0);
-	}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 
