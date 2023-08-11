1f5f8931aced96cb49efe830dc45511e3db5b296
belloraheemah <belloraheemah1@gmail.com>
2023-08-11 04:05:00 -0700

    Puts and size corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 	/**
-	 * main- Entry point
+	 * main - Entry point
 	 *
 	 * Description- program that print
 	 * Return: Always 0 (success)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 	/**
-	 * main- entry point
+	 * main - entry point
 	 *
 	 * description- print the size of various types
 	 * Return: 0 Always (success)
