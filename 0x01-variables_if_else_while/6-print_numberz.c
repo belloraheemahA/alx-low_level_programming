a1947436ab74de41986b60703cfab8cd9dda8e23
belloraheemah <belloraheemah1@gmail.com>
2023-08-12 14:52:05 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints numbers
+ * Description - This program displays numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
