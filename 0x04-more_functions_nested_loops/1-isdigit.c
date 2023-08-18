b69ea2c396d262a5d5540cd2e7b00481eb7274bc
belloraheemah <belloraheemah1@gmail.com>
2023-08-18 16:16:07 -0700

    isdigit andmul

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks digit
+ * @c: is the char to be checked
+ * Return: 1 if char is digit, otherwise 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+else
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul -  multiplcaation ttwoo intt
+ * @a: first int
+ * @b: second int
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
